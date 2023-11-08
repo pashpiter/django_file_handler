a46e8b718cfa4b8e0f032637446b7a53a0111652
Pavel Drovnin <pavel98rus@gmail.com>
2023-11-08 19:21:17 +0300

    views test case

---
+++ file_handler/tests/test_views.py
@@ -0,0 +1,58 @@
+import os
+import shutil
+import tempfile
+
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.test import Client, TestCase, override_settings
+from rest_framework.response import Response
+
+from file_handler.settings import BASE_DIR
+
+TEMP_MEDIA_ROOT = tempfile.mkdtemp(dir=os.path.join(BASE_DIR, 'tmp'))
+
+
+@override_settings(MEDIA_ROOT=TEMP_MEDIA_ROOT)
+@override_settings(CELERY_TASK_ALWAYS_EAGER=True)
+class FileViewsTest(TestCase):
+    """Тесты для View функций"""
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        shutil.rmtree(TEMP_MEDIA_ROOT, ignore_errors=True,)
+
+    def setUp(self) -> None:
+        self.client = Client()
+
+    def test_ping(self):
+        response: Response = self.client.get('')
+        assert response.status_code == 200
+        assert response.data == {'app': 'django_file_handler',
+                                 'version': '1.0'}
+
+    def test_post_empty_data(self):
+        empty_data = {}
+        response: Response = self.client.post('/upload/', data=empty_data)
+        assert response.status_code == 400
+        assert response.data['message'] == 'No file recieved'
+
+    def test_post_image_file(self):
+        with open(os.path.join(BASE_DIR, 'tests/apple.png'), 'rb') as f:
+            data = {'file': f}
+            response: Response = self.client.post('/upload/', data=data)
+        assert response.status_code == 201
+        assert response.data['file'] == '/media/uploaded_files/apple.jpeg'
+
+    def test_post_text_file(self):
+        f = SimpleUploadedFile('test.txt', b'test content', 'text/plain')
+        data = {'file': f}
+        response: Response = self.client.post('/upload/', data=data)
+        assert response.status_code == 201
+        assert response.data['file'] == '/media/uploaded_files/test.txt'
+
+    def test_invalid_post(self):
+        f = SimpleUploadedFile('test.gif', b'test content', 'image/gif')
+        data = {'file': f}
+        response: Response = self.client.post('/upload/', data=data)
+        assert response.status_code == 403
+        assert 'not allowed' in response.data['message'].lower()
